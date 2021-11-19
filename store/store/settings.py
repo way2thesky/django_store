55f59250af4e649c6f6687aea56c99581a6b072b
way2thesky <voitko.serhii@gmail.com>
2021-11-20 01:16:26 +0200

    some fixes

--- store/store/settings.py
+++ store/store/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dbshop',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgdb',
-#         'HOST': 'db_shop',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbshop',
+        'USER': 'postgres',
+        'PASSWORD': 'postgdb',
+        'HOST': 'db_shop',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
